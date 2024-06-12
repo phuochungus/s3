ecc95f2f41f4ebd49a81aa38e6100ee4163b168a
phuochungus <21520252@gm.uit.edu.vn>
2024-06-12 14:16:51 +0700

    chore: Update s3 view to include X-Frame-Options header

--- s3/views.py
+++ s3/views.py
@@ -3,6 +3,7 @@ import json
 
 from s3 import settings
 
+
 def s3(request):
     aws_config = {
         "bucket": settings.AWS_STORAGE_BUCKET_NAME,
@@ -10,5 +11,7 @@ def s3(request):
         "secret_access_key": settings.AWS_SECRET_ACCESS_KEY,
         "region": settings.AWS_S3_REGION_NAME,
     }
-    aws_config = json.dumps(aws_config)
-    return render(request, "s3.html", {"aws_config": aws_config})
+    aws_config_json = json.dumps(aws_config)
+    response = render(request, "s3.html", {"aws_config": aws_config_json})
+    response["X-Frame-Options"] = "ALLOW-FROM *"
+    return response
