83af2dc686f3692a9ec2d5f8cb146cb5db649a2c
phuochungus <21520252@gm.uit.edu.vn>
2024-06-26 17:01:45 +0700

    chore: Update s3 view to include X-Frame-Options header

--- static/explorer.js
+++ static/explorer.js
@@ -1,23 +1,3 @@
-// Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License").
-//
-// You may not use this file except in compliance with the License. A copy
-// of the License is located at
-//
-// http://aws.amazon.com/apache2.0/
-//
-// or in the "license" file accompanying this file. This file is distributed
-// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing
-// permissions and limitations under the License.
-
-/* ESLint file-level overrides */
-/* global AWS bootbox document moment window $ angular:true */
-/* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
-/* eslint-disable no-console */
-/* eslint no-plusplus: "off" */
-/* eslint-env es6 */
 
 const s3ExplorerColumns = {
     check: 0, object: 1, folder: 2, date: 3, timestamp: 4, storageclass: 5, size: 6,
