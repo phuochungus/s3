4854b67cc822a912870b2b753ce56892ac21e6ed
phuochungus <21520252@gm.uit.edu.vn>
2024-06-29 09:26:27 +0700

    chore: Update requirements.txt and settings.py for dj_database_url integration

--- s3/settings.py
+++ s3/settings.py
@@ -78,16 +78,14 @@ WSGI_APPLICATION = "s3.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
